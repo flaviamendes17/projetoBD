5976a5b20fc363a4f1c48c0651e3ed5c2e1d392b
Luiz Gabriel <luiz.gabriellopesc@gmail.com>
2024-11-26 15:33:05 -0300

    feat: add noticias table

--- luizgabriel.sql
+++ luizgabriel.sql
@@ -1,1 +1,7 @@
-/*Júlia Guarnieri*/
+CREATE TABLE noticias (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descricao VARCHAR(200) NOT NULL,
+    imagem_url TEXT NOT NULL,
+    autor VARCHAR(100) NOT NULL
+);
